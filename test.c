952b97f33f981e3187eba3f5c6eee2f585d68060
franu235 <87235076+franu235@users.noreply.github.com>
2021-07-11 19:59:46 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 int my_name();
+int my_age();
 int main()
 {
+  int a;
   printf("hello world");
   my_name();
+  a = my_age();
+  printf("my age is:%d",a);
   return(0);
 }
 int my_name()
 {
   printf("suraj dhiman");
+}
+int my_age()
+{
+  int age;
+  printf("enter age:");
+  scanf("%d",&age);
+  return age;
 }
